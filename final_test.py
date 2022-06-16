ff829132e812b903cda79d1c93ba68db7502d5db
Shreyans Daga <shreyansdaga007@gmail.com>
2022-06-16 19:47:14 -0400

    Cleaned up final_test.py

--- final_test.py
+++ final_test.py
@@ -1,52 +1,36 @@
-#!/usr/bin/python3
-import time
 import logging
-import time
-import os
 import cv2
 from picamera2 import Picamera2, Preview, MappedArray
 
-# This version creates a lores YUV stream, extracts the Y channel and runs the face
-# detector directly on that. We use the supplied OpenGL accelerated preview window
-# and delegate the face box drawing to its callback function, thereby running the
-# preview at the full rate with face updates as and when they are ready.
-
-def draw_faces(request):
-    pass
-
-
+# Configure camera image types
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
 config = picam2.preview_configuration(main={"size": (640, 480)},
                                       lores={"size": (320, 240), "format": "YUV420"})
 picam2.configure(config)
 
+# Configure camera to be able to take pictures
 (w0, h0) = picam2.stream_configuration("main")["size"]
 (w1, h1) = picam2.stream_configuration("lores")["size"]
 s1 = picam2.stream_configuration("lores")["stride"]
-picam2.post_callback = draw_faces
-
 picam2.start()
 
-start_time = time.monotonic()
-# Run for 10 seconds so that we can include this example in the test suite.
 while True:
+    # Get color and convert to gray-scale
     buffer = picam2.capture_buffer("lores")
     grey = buffer[:s1 * h1].reshape((h1, s1))
-    cv2.imshow("grey", grey)
 
+    # Convert gray-scale to binary
     ret, thresh = cv2.threshold(grey, 150, 255, cv2.THRESH_BINARY)
-    cv2.imshow('Binary image', thresh)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.waitKey(500)
 
+    # Get contours in image 
     contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-    image_copy = thresh.copy()
+    
+    # Convert grey-scale into RGB image to display contours
     color = cv2.cvtColor(grey, cv2.COLOR_GRAY2RGB)
-    cv2.imshow("color", color)
     cv2.drawContours(image=color, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
-    cv2.imshow("contours_drawn", color)
-    cv2.waitKey(0)
+    # Save image with contours drawn on top
+    cv2.waitKey(500)
     cv2.imwrite('contours.jpg', color)
-    cv2.destroyAllWindows()
+    break
